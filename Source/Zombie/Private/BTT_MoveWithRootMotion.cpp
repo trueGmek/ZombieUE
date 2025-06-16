765878ac33c6a9515c184470ad9414da8a5e0781
trueGmek <trueGmek0204@gmail.com>
2025-06-16 15:46:20 +0200

    Root motion movement reached an acceptable state

--- Source/Zombie/Private/BTT_MoveWithRootMotion.cpp
+++ Source/Zombie/Private/BTT_MoveWithRootMotion.cpp
@@ -1,10 +1,15 @@
 #include "BTT_MoveWithRootMotion.h"
 #include "AIController.h"
 #include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "DrawDebugHelpers.h"
 #include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "NavigationSystem.h"
 
 UBTT_MoveWithRootMotion::UBTT_MoveWithRootMotion() {
   bNotifyTick = true;
@@ -14,6 +19,14 @@ UBTT_MoveWithRootMotion::UBTT_MoveWithRootMotion() {
   Destination.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UBTT_MoveWithRootMotion, Destination));
   Destination.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTT_MoveWithRootMotion, Destination),
                               AActor::StaticClass());
+}
+
+void UBTT_MoveWithRootMotion::InitializeFromAsset(UBehaviorTree& Asset) {
+  Super::InitializeFromAsset(Asset);
+
+  if (Asset.BlackboardAsset) {
+    Destination.ResolveSelectedKey(*Asset.BlackboardAsset);
+  }
 }
 
 FString UBTT_MoveWithRootMotion::GetStaticDescription() const {
@@ -41,47 +54,78 @@ EBTNodeResult::Type UBTT_MoveWithRootMotion::ExecuteTask(UBehaviorTreeComponent&
   ZombieAnimInstance = Cast<UZombieAnimInstance>(ZombieCharacter->GetMesh()->GetAnimInstance());
   ensureMsgf(ZombieAnimInstance, TEXT("The AnimInstance is not of type ZombieAnimInstance"));
 
+  CurrentPath = GetPathToDestination(OwnerComp, ZombieCharacter->GetActorLocation());
+
+  if (!CurrentPath || CurrentPath->PathPoints.Num() == 0) {
+    UE_LOG(LogTemp, Warning, TEXT("[%hs]: Generated path is null or has no PathPoints inside"), __PRETTY_FUNCTION__)
+    return EBTNodeResult::Failed;
+  }
+
+  PathPointIndex = 0;
   ZombieAnimInstance->bShouldWalk = this->MovementType == EMovementType::Walk;
   ZombieAnimInstance->bShouldRun = this->MovementType == EMovementType::Run;
 
   return EBTNodeResult::InProgress;
 }
 
-void UBTT_MoveWithRootMotion::InitializeFromAsset(UBehaviorTree& Asset) {
-  Super::InitializeFromAsset(Asset);
-
-  if (Asset.BlackboardAsset) {
-    Destination.ResolveSelectedKey(*Asset.BlackboardAsset);
-  }
-}
-
 void UBTT_MoveWithRootMotion::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) {
-  ensureMsgf(Destination.IsSet(), TEXT("Destination.IsSet was false!"));
-  AActor* agent = OwnerComp.GetOwner();
-  const FVector destinationLocation = GetDestinationLocation(OwnerComp);
-
-  const FVector currentLocation = agent->GetActorLocation();
-
-  FVector direction = (destinationLocation - currentLocation).GetSafeNormal();
-  FQuat DesiredRot = direction.Rotation().Quaternion();
-
-  // 2. Smoothly interpolate from current to desired
-  FQuat CurrentRot = agent->GetActorQuat();
-  FQuat NewRot = FMath::QInterpTo(CurrentRot, DesiredRot, DeltaSeconds, RotationSpeed.GetValue(Blackboard));
-  agent->SetActorRotation(NewRot);
-  if ((destinationLocation - currentLocation).Length() <= AcceptanceRange.GetValue(Blackboard)) {
-    FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+  if (ZombieCharacter == nullptr || !CurrentPath) {
+    FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+    return;
   }
+
+  const FVector CurrentLocation = ZombieCharacter->GetActorLocation();
+  const FVector FinalDestination = CurrentPath->PathPoints.Last();
+  const FVector ProjCurrentLocation =
+      UKismetMathLibrary::ProjectVectorOnToPlane(CurrentLocation, FVector::UpVector);
+
+  if (FVector::DistSquared(ProjCurrentLocation, FinalDestination) < FMath::Square(AcceptanceRange.GetValue(Blackboard))) {
+    FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+    return;
+  }
+
+  const FVector TargetPoint = CurrentPath->PathPoints[PathPointIndex];
+
+  if (FVector::DistSquared(ProjCurrentLocation, TargetPoint) < FMath::Square(NextPointRadius.GetValue(Blackboard))) {
+    PathPointIndex++;
+    if (PathPointIndex >= CurrentPath->PathPoints.Num()) {
+      // This case should be caught by the final destination check, but as a fallback
+      FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+      return;
+    }
+  }
+
+  // Smoothly rotate the actor to face the current target point
+  const FRotator CurrentRotation = ZombieCharacter->GetActorRotation();
+  const FRotator TargetRotation =
+      UKismetMathLibrary::FindLookAtRotation(CurrentLocation, CurrentPath->PathPoints[PathPointIndex]);
+  const FRotator NewRotation =
+      FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaSeconds, RotationSpeed.GetValue(Blackboard));
+
+  // Only update Yaw to prevent the character from tilting up/down
+  ZombieCharacter->SetActorRotation(FRotator(0.F, NewRotation.Yaw, 0.F));
 }
 
-FVector UBTT_MoveWithRootMotion::GetDestinationLocation(UBehaviorTreeComponent& OwnerComp) {
+void UBTT_MoveWithRootMotion::OnTaskFinished(UBehaviorTreeComponent& OwnerComp,
+                                             uint8* NodeMemory,
+                                             EBTNodeResult::Type TaskResult) {
 
+  UE_LOG(LogTemp, Display, TEXT("Stoping movement. Disabling the animation flags"));
+  ZombieAnimInstance->bShouldWalk = false;
+  ZombieAnimInstance->bShouldRun = false;
+  this->PathPointIndex = 0;
+}
+
+UNavigationPath* UBTT_MoveWithRootMotion::GetPathToDestination(UBehaviorTreeComponent& OwnerComp,
+                                                               const FVector CurrentPosition) {
   if (Destination.SelectedKeyType == UBlackboardKeyType_Object::StaticClass()) {
     UObject* KeyValue = Blackboard->GetValue<UBlackboardKeyType_Object>(Destination.GetSelectedKeyID());
+
+    UE_LOG(LogTemp, Display, TEXT("Destination name: %s"), *Destination.SelectedKeyName.GetPlainNameString());
     AActor* TargetActor = Cast<AActor>(KeyValue);
 
     if (TargetActor != nullptr) {
-      return TargetActor->GetActorLocation();
+      return UNavigationSystemV1::FindPathToActorSynchronously(GetWorld(), CurrentPosition, TargetActor);
     }
     UE_LOG(LogTemp, Error,
            TEXT("Destination key was set to an object that is null or not an Actor! Please make sure  that the value "
@@ -89,10 +133,11 @@ FVector UBTT_MoveWithRootMotion::GetDestinationLocation(UBehaviorTreeComponent& 
   }
   if (Destination.SelectedKeyType == UBlackboardKeyType_Vector::StaticClass()) {
 
-    return Blackboard->GetValue<UBlackboardKeyType_Vector>(Destination.GetSelectedKeyID());
+    FVector destination = Blackboard->GetValue<UBlackboardKeyType_Vector>(Destination.GetSelectedKeyID());
+    return UNavigationSystemV1::FindPathToLocationSynchronously(GetWorld(), CurrentPosition, destination);
   }
 
   UE_LOG(LogTemp, Error, TEXT("Something went wrong! Quiting the task!"));
   FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
-  return FVector::ZeroVector;
+  return nullptr;
 }
--- Source/Zombie/Public/BTT_MoveWithRootMotion.h
+++ Source/Zombie/Public/BTT_MoveWithRootMotion.h
@@ -4,6 +4,7 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "BehaviorTree/ValueOrBBKey.h"
 #include "CoreMinimal.h"
+#include "NavigationPath.h"
 #include "ZombieAnimInstance.h"
 #include "ZombieCharacter.h"
 
@@ -15,6 +16,11 @@ enum class EMovementType : uint8 {
   Run UMETA(DisplayName = "Run"),
 };
 
+/**
+ * Custom BT Task to move an actor using Root Motion.
+ * It requests a path, follows it, smoothly rotates the actor,
+ * and sets animation flags to drive the movement.
+ */
 UCLASS()
 class ZOMBIE_API UBTT_MoveWithRootMotion : public UBTTaskNode {
 
@@ -33,11 +39,20 @@ protected:
   UPROPERTY(Category = Blackbaoard, EditAnywhere, meta = (ClampMin = "1", UIMin = "1"))
   FValueOrBBKey_Float AcceptanceRange;
 
+  UPROPERTY(Category = Blackbaoard, EditAnywhere, meta = (ClampMin = "1", UIMin = "1"))
+  FValueOrBBKey_Float NextPointRadius{50.0F};
+
   UPROPERTY()
   TObjectPtr<UZombieAnimInstance> ZombieAnimInstance;
 
   UPROPERTY()
   TObjectPtr<AZombieCharacter> ZombieCharacter;
+
+  UPROPERTY()
+  TObjectPtr<UNavigationPath> CurrentPath;
+
+  /** Index of the current point on the path the actor is moving towards */
+  int32 PathPointIndex{0};
 
 private:
   TObjectPtr<UBlackboardComponent> Blackboard;
@@ -49,7 +64,8 @@ public:
   virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
   virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
   virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+  void OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTNodeResult::Type TaskResult) override;
 
 private:
-  FVector GetDestinationLocation(UBehaviorTreeComponent& OwnerComp);
+ UNavigationPath* GetPathToDestination(UBehaviorTreeComponent& OwnerComp, FVector CurrentPosition);
 };
